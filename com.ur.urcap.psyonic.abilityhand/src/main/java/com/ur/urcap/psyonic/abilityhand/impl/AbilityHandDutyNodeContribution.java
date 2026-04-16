cfd3a792d08e532ac472ee653512d41ecb5d2b30
Sam Gossett <sgossett2020@gmail.com>
2026-04-16 16:49:05 -0400

    decreased duty sleep from 0.5 to 0.2, might need to remove all together

--- com.ur.urcap.psyonic.abilityhand/src/main/java/com/ur/urcap/psyonic/abilityhand/impl/AbilityHandDutyNodeContribution.java
+++ com.ur.urcap.psyonic.abilityhand/src/main/java/com/ur/urcap/psyonic/abilityhand/impl/AbilityHandDutyNodeContribution.java
@@ -90,7 +90,7 @@ public class AbilityHandDutyNodeContribution implements ProgramNodeContribution 
         "])"
         );
 
-        writer.appendLine("sleep(0.5)");
+        writer.appendLine("sleep(0.2)");
         writer.appendLine("ah_daemon.startPositionThread()");
         
     }
